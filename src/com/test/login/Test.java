2961d56b7a4e3630c180e14828df56835efeaec6
nachniki <NRout@altimetrik.com>
2016-08-20 22:29:59 +0530

    change done
    change done in the file

--- src/com/test/login/Test.java
+++ src/com/test/login/Test.java
@@ -1,5 +1,13 @@
 package com.test.login;
 
 public class Test {
+Test(){
+System.out.println("main method Test classesssssssssss");
+}
+
+public static void main(String arg[]){
+System.out.println("main method Test class");
+
+}
 
 }
